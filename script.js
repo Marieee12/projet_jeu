d04cb60ffc7b4ca5def938ece136f141ff6eff2f
EmiLy-Ly-san <emilie.deduyver@gmail.com>
2026-01-31 15:28:46 +0100

    logs globals front

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ import { bindModals } from "./ui/modals.js";
 import { createGameController } from "./game/loop.js";
 import { renderLandingLeaderboard } from "./ui/landingleaderboard.js";
 
+import { logInfo, logError } from "./logger.js";
+
+logInfo("app_loaded", {
+  screen: "landing",
+});
+
 function hideLoader() {
+  logInfo("loader_hidden");
+
   dom.loaderScreen.classList.add("hidden");
   dom.landingPage.classList.remove("hidden");
 
@@ -29,6 +37,7 @@ const modals = bindModals(dom, {
   },
   onRestart: () => gameController.initializeGame({ showIntro: false }),
   onQuit: () => {
+    logInfo("game_quit_clicked");
     gameController.stopGame();
     dom.gameScreen.classList.add("hidden");
     dom.landingPage.classList.remove("hidden");
@@ -37,6 +46,7 @@ const modals = bindModals(dom, {
   },
   onWinRestart: () => gameController.initializeGame({ showIntro: false }),
   onWinQuit: () => {
+    logInfo("game_win_quit_clicked");
     gameController.stopGame();
     dom.gameScreen.classList.add("hidden");
     dom.landingPage.classList.remove("hidden");
@@ -48,6 +58,9 @@ const gameController = createGameController(dom, modals);
 gameController.bindInputs();
 
 dom.startButton?.addEventListener("click", () => {
+  logInfo("game_start_clicked", {
+    hasPlayerName: !!getPlayerName(),
+  });
   if (!getPlayerName()) return;
   dom.landingPage.classList.add("hidden");
   dom.gameScreen.classList.remove("hidden");
@@ -56,6 +69,22 @@ dom.startButton?.addEventListener("click", () => {
 
 // loader
 setTimeout(hideLoader, 2000);
+
+// LOGS des erreurs globales front
+window.addEventListener("error", (e) => {
+  logError("frontend_error", {
+    message: e.message,
+    file: e.filename,
+    line: e.lineno,
+    column: e.colno,
+  });
+});
+
+window.addEventListener("unhandledrejection", (e) => {
+  logError("promise_rejection", {
+    reason: String(e.reason),
+  });
+});
 
 
 
